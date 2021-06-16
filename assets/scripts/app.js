a6e1ba093872fd98f78bac0c109d385361af33ac
Somadevv <somadev@hotmail.co.uk>
2021-06-16 01:03:04 +0100

    Added all elements to app.js

---
+++ assets/scripts/app.js
@@ -0,0 +1,456 @@
+var elements = [
+    {
+        name: 'rain',
+        cost: 40000,
+        src: 'assets/images/elements/rain.png'
+    },
+    {
+        name: 'sea',
+        cost: 40000,
+        src: 'assets/images/elements/sea.png'
+    },
+    {
+        name: 'mud',
+        cost: 40000,
+        src: 'assets/images/elements/mud.png'
+    },
+    {
+        name: 'dust',
+        cost: 40000,
+        src: 'assets/images/elements/dust.png'
+    },
+    {
+        name: 'pressure',
+        cost: 40000,
+        src: 'assets/images/elements/pressure.png'
+    },
+    {
+        name: 'lava',
+        cost: 40000,
+        src: 'assets/images/elements/lava.png'
+    },
+    {
+        name: 'energy',
+        cost: 40000,
+        src: 'assets/images/elements/energy.png'
+    },
+    {
+        name: 'steam',
+        cost: 40000,
+        src: 'assets/images/elements/steam.png'
+    },
+    {
+        name: 'wind',
+        cost: 130000,
+        src: 'assets/images/elements/wind.png'
+    },
+    {
+        name: 'flood',
+        cost: 160000,
+        src: 'assets/images/elements/flood.png'
+    },
+    {
+        name: 'plant',
+        cost: 160000,
+        src: 'assets/images/elements/plant.png'
+    },
+    {
+        name: 'ocean',
+        cost: 160000,
+        src: 'assets/images/elements/ocean.png'
+    },
+    {
+        name: 'brick',
+        cost: 100000,
+        src: 'assets/images/elements/ocean.png'
+    },
+    {
+        name: 'gunpowder',
+        cost: 130000,
+        src: 'assets/images/elements/ocean.png'
+    },
+    {
+        name: 'atmosphere',
+        cost: 130000,
+        src: 'assets/images/elements/ocean.png'
+    },
+    {
+        name: 'obsidian',
+        cost: 130000,
+        src: 'assets/images/elements/ocean.png'
+    },
+    {
+        name: 'stone',
+        cost: 100000,
+        src: 'assets/images/elements/ocean.png'
+    },
+    {
+        name: 'earthquake',
+        cost: 100000,
+        src: 'assets/images/elements/ocean.png'
+    },
+    {
+        name: 'cloud',
+        cost: 310000,
+        src: 'assets/images/elements/ocean.png'
+    },
+    {
+        name: 'geyser',
+        cost: 190000,
+        src: 'assets/images/elements/ocean.png'
+    },
+    {
+        name: 'wave',
+        cost: 400000,
+        src: 'assets/images/elements/ocean.png'
+    },
+    {
+        name: 'hurricane',
+        cost: 520000
+    },
+    {
+        name: 'volcano',
+        cost: 490000
+    },
+    {
+        name: 'tobacco',
+        cost: 340000
+    },
+    {
+        name: 'swamp',
+        cost: 250000
+    },
+    {
+        name: 'moss',
+        cost: 280000
+    },
+    {
+        name: 'grass',
+        cost: 250000
+    },
+    {
+        name: 'coal',
+        cost: 280000
+    },
+    {
+        name: 'wall',
+        cost: 340000
+    },
+    {
+        name: 'explosion',
+        cost: 310000
+    },
+    {
+        name: 'ash',
+        cost: 610000
+    },
+    {
+        name: 'metal',
+        cost: 670000
+    },
+    {
+        name: 'sand',
+        cost: 460000
+    },
+    {
+        name: 'mountain',
+        cost: 640000
+    },
+    {
+        name: 'storm',
+        cost: 730000
+    },
+    {
+        name: 'fog',
+        cost: 370000
+    },
+    {
+        name: 'sky',
+        cost: 580000
+    },
+    {
+        name: 'sound',
+        cost: 820000
+    },
+    {
+        name: 'isle',
+        cost: 670000
+    },
+    {
+        name: 'diamond',
+        cost: 400000
+    },
+    {
+        name: 'archipelago',
+        cost: 2050000
+    },
+    {
+        name: 'fireworks',
+        cost: 1840000
+    },
+    {
+        name: 'grenade',
+        cost: 1240000
+    },
+    {
+        name: 'steel',
+        cost: 970000
+    },
+    {
+        name: 'ring',
+        cost: 1870000
+    },
+    {
+        name: 'bullet',
+        cost: 1600000
+    },
+    {
+        name: 'blade',
+        cost: 1120000
+    },
+    {
+        name: 'electricity',
+        cost: 1420000
+    },
+    {
+        name: 'river',
+        cost: 1030000
+    },
+    {
+        name: 'glass',
+        cost: 490000
+    },
+    {
+        name: 'boiler',
+        cost: 760000
+    },
+    {
+        name: 'rust',
+        cost: 700000
+    },
+    {
+        name: 'sun',
+        cost: 610000
+    },
+    {
+        name: 'cactus',
+        cost: 1360000
+    },
+    {
+        name: 'dew',
+        cost: 640000
+    },
+    {
+        name: 'life',
+        cost: 280000
+    },
+    {
+        name: 'horizon',
+        cost: 1540000
+    },
+    {
+        name: 'desert',
+        cost: 2560000
+    },
+    {
+        name: 'beach',
+        cost: 1390000
+    },
+    {
+        name: 'alcohol',
+        cost: 190000
+    },
+    {
+        name: 'compass',
+        cost: 2320000
+    },
+    {
+        name: 'sand clock',
+        cost: 2080000
+    },
+    {
+        name: 'time',
+        cost: 4720000
+    },
+    {
+        name: 'clay',
+        cost: 640000
+    },
+    {
+        name: 'tree',
+        cost: 790000
+    },
+    {
+        name: 'knife',
+        cost: 3820000
+    },
+    {
+        name: 'bottle',
+        cost: 550000
+    },
+    {
+        name: 'heat',
+        cost: 40000
+    },
+    {
+        name: 'wheat',
+        cost: 820000
+    },
+    {
+        name: 'tinplate',
+        cost: 790000
+    },
+    {
+        name: 'bulb',
+        cost: 3250000
+    },
+    {
+        name: 'carrot',
+        cost: 220000
+    },
+    {
+        name: 'cabbage',
+        cost: 190000
+    },
+    {
+        name: 'cannabis',
+        cost: 1390000
+    },
+    {
+        name: 'garbage',
+        cost: 40000
+    },
+    {
+        name: 'pot',
+        cost: 1660000
+    },
+    {
+        name: 'gun',
+        cost: 4480000
+    },
+    {
+        name: 'radiation',
+        cost: 40000
+    },
+    {
+        name: 'atomic bomb',
+        cost: 3370000
+    },
+    {
+        name: 'dark glass',
+        cost: 280000
+    },
+    {
+        name: 'glasses',
+        cost: 1900000
+    },
+    {
+        name: 'flour',
+        cost: 2500000
+    },
+    {
+        name: 'bread',
+        cost: 4210000
+    },
+    {
+        name: 'tsunami',
+        cost: 1060000
+    },
+    {
+        name: 'alarm',
+        cost: 7180000
+    },
+    {
+        name: "Virus",
+        cost: 730000,
+        image: "assets/images/elements/virus.png",
+      },
+      {
+        name: "Black Matter",
+        cost: 1000000,
+        image: "assets/images/elements/black_matter.png",
+      },
+      {
+        name: "Black Hole",
+        cost: 3160000,
+        image: "assets/images/elements/black_hole.png",
+      },
+      {
+        name: "Plasma",
+        cost: 370000,
+        image: "assets/images/elements/plasma.png",
+      },
+      {
+        name: "Chain",
+        cost: 1030000,
+        image: "assets/images/elements/chain.png",
+      },
+      {
+        name: "Shit",
+        cost: 5410000,
+        image: "assets/images/elements/shit.png",
+      },
+      {
+        name: "Gold",
+        cost: 21640000,
+        image: "assets/images/elements/gold.png",
+      },
+      {
+        name: "Lens",
+        cost: 1030000,
+        image: "assets/images/elements/lens.png",
+      },
+      {
+        name: "Microscope",
+        cost: 2860000,
+        image: "assets/images/elements/microscope.png",
+      },
+      {
+        name: "Prism",
+        cost: 4900000,
+        image: "assets/images/elements/prism.png",
+      },
+      {
+        name: "Smoke",
+        cost: 380000,
+        image: "assets/images/elements/smoke.png",
+      },
+      {
+        name: "Pipe",
+        cost: 5290000,
+        image: "assets/images/elements/pipe.png",
+      },
+      {
+        name: "Reed Pipe",
+        cost: 5560000,
+        image: "assets/images/elements/reed_pipe.png",
+      },
+      {
+        name: "Music",
+        cost: 12760000,
+        image: "assets/images/elements/music.png",
+      },
+      {
+        name: "Money",
+        cost: 436900000,
+        image: "assets/images/elements/money.png",
+      },
+      {
+        name: "Blockchain",
+        cost: 3131000,
+        image: "assets/images/elements/blockchain.png",
+      },
+      {
+        name: "Block",
+        cost: 760000,
+        image: "assets/images/elements/block.png",
+      },
+      {
+        name: "Broom",
+        cost: 4930000,
+        image: "assets/images/elements/broom.png",
+      },]
+ 
+
+
+   
